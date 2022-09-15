9b02bb705404aab4e0391168c392c08530b877d1
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:11:40 +0530

    Create Scalar Multiplication.cpp

---
+++ Week 2/Arrays/Scalar Multiplication.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    int r,c,k;
+    cin>>r>>c>>k;
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+      for(int j=0;j<c;j++){
+        cin>>arr[i][j];
+        arr[i][j]*=k;
+      }
+    }
+    for(int i=0;i<r;i++){
+      for(int j=0;j<c;j++){
+        cout<<arr[i][j]<<" ";
+      }
+      cout<<endl;
+    }
+    return 0;
+  }
