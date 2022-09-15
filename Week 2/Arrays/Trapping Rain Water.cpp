d45f60e8ee5ccb17045ca72713fe7fadd0bcdcef
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:17:31 +0530

    Create Trapping Rain Water.cpp

---
+++ Week 2/Arrays/Trapping Rain Water.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    int t;
+    cin>>t;
+    while(t--){
+      long long n;
+      cin>>n;
+      long long arr[n];
+      for(int i=0;i<n;i++){
+        cin>>arr[i];
+      }
+      int max1=0;
+      int max2=0;
+      
+    }
+    
+    return 0;
+  }
