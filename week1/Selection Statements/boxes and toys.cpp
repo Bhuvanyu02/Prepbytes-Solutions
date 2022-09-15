9f9da14af4dad81734f5f8ac471bcbf2a3ec1c6c
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:02:44 +0530

    Create boxes and toys.cpp

---
+++ week1/Selection Statements/boxes and toys.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int t;
+    cin>>t;
+    int count=0;
+    while(t--){
+      int T,c;
+      cin>>T>>c;
+      if(c-T>=2){
+        count++;
+        }
+        
+    }
+    cout<<count<<endl;
+    return 0;
+  }
