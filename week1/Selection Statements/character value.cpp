b7008d5a700f00f46f9cf34a6afdd81410c20361
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:01:37 +0530

    Create character value.cpp

---
+++ week1/Selection Statements/character value.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    char ch;
+    cin>>ch;
+    switch(ch){
+      case 'P':
+      case 'p':
+            cout<<"PrepBytes"<<endl;
+            break;
+      case 'Z':
+      case 'z':
+            cout<<"Zenith"<<endl;
+            break;
+      case 'e':
+      case 'E':
+            cout<<"Expert Coder"<<endl;
+            break;
+      case 'd':
+      case 'D':
+            cout<<"Data Structure"<<endl;
+            break;
+      
+    }
+    return 0;
+  }
