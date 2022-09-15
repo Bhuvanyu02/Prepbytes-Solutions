36baf6aeb159a9560577b41911da957a0e0a16f2
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:21:45 +0530

    Create Decreasing Order Strings.cpp

---
+++ Strings/Decreasing Order Strings.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    int t;
+    cin>>t;
+    while(t--){
+      string s;
+      cin>>s;
+      int l=s.length();
+      for(int i=0;i<l;i++){
+        for(int j=0;j<l;j++){
+          if(s[j]<s[i]){
+            char temp;
+            temp=s[j];
+            s[j]=s[i];
+            s[i]=temp;
+          }
+        }
+      }
+      cout<<s<<endl;
+    }
+    
+    return 0;
+  }
