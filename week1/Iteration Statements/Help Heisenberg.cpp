d049127932fea8ae6b704d71ef402174b244c288
Bhuvanyu02 <78157106+Bhuvanyu02@users.noreply.github.com>
2022-09-15 20:07:46 +0530

    Create Help Heisenberg.cpp

---
+++ week1/Iteration Statements/Help Heisenberg.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   long long n;
+   cin>>n;
+  long long r = (n*(n+1))/2;
+   if(r%2==0){
+     cout<<"0"<<endl;
+    
+   }
+   else{
+     cout<<"1"<<endl;
+    
+   }
+  return 0;
+}
